43de1d830bc84910fc204500d3091493f31567f0
Vergueirod <diego.ssv@outlook.com>
2025-03-04 11:07:02 -0300

    Initial commit

---
+++ src/main/java/com/vergueiro_group/notifyhub_backed_notification/NotifyhubBackedNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.vergueiro_group.notifyhub_backed_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyhubBackedNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifyhubBackedNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vergueiro_group/notifyhub_backed_notification/NotifyhubBackedNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vergueiro_group.notifyhub_backed_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyhubBackedNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
